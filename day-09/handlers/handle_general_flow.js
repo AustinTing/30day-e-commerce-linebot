80b09d90efc256ff31e0d380468f8d803aee62ef
CellBody <austintingwork@gmail.com>
2018-10-11 21:57:29 +0800

    Update handleGetProducts

--- day-09/handlers/handle_general_flow.js
+++ day-09/handlers/handle_general_flow.js
@@ -1,4 +1,6 @@
 const { LineHandler } = require('bottender')
+const Product = require('../database/product.js')
+const _ = require('lodash')
 
 const isGetOrders = context => {
   const {event} = context
@@ -16,8 +18,41 @@ const isGetProducts = context => {
   return false
 }
 
-const handleGetProducts = context => {
-  context.replyText('You click getProducts')
+const toColumns = products => {
+  return _.map(products, product => {
+    return {
+      thumbnailImageUrl: product.imgUrl,
+      imageBackgroundColor: '#FFFFFF',
+      title: product.name,
+      text: product.detail,
+      defaultAction: {
+        type: 'uri',
+        label: 'View image',
+        uri: product.imgUrl
+      },
+      actions: [
+        {
+          type: 'postback',
+          label: '我想要買！',
+          data: `flow=shoping&action=buy&productID=${product.id}`
+        }
+      ]
+    }
+  })
+}
+
+const handleGetProducts = async context => {
+  const products = await Product.readProducts()
+  let carouselProducts = {
+    type: 'template',
+    altText: '好吃好吃嚼嚼嚼',
+    imageAspectRatio: 'rectangle',
+    imageSize: 'cover',
+    template: {
+      type: 'carousel',
+      columns: toColumns(products)
+    }}
+  context.reply([carouselProducts])
 }
 const isGetMemberInfo = context => {
   const {event} = context
