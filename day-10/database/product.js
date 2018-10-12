4e15797d4ba08b11d4edeabab1d2ac13eecf5532
CellBody <austintingwork@gmail.com>
2018-10-12 23:49:44 +0800

    Add pagination in product list

--- day-10/database/product.js
+++ day-10/database/product.js
@@ -1,17 +1,19 @@
 const airtable = require('./airtable.js')
 const _ = require('lodash')
 
-module.exports.readProducts = async () => {
-  const result = await airtable.get({
+module.exports.readProducts = async ({offset} = {}) => {
+  let result = await airtable.get({
     table: 'product',
-    pageSize: 10,
+    offset,
+    pageSize: 9,
     sort: [{field: 'id', direction: 'asc'}]
   })
-  console.log(JSON.stringify(result))
-  return _.compact(_.map(result.records, record => {
+  result['products'] = _.compact(_.map(result.records, record => {
     const {fields} = record
     const {id, name, detail, price, images} = fields
     if (!(name && detail && price && images && images[0].url)) return console.log(`Some data in product id: ${id} is not correct`)
     return { id, name, detail, price, imgUrl: images[0].url }
   }))
+  console.log(JSON.stringify(result))
+  return result
 }
--- day-10/handlers/handle_general_flow.js
+++ day-10/handlers/handle_general_flow.js
@@ -2,14 +2,82 @@ const { LineHandler } = require('bottender')
 const Product = require('../database/product.js')
 const _ = require('lodash')
 
-const isGetOrders = context => {
-  const {event} = context
-  if (event.postback.query.action && event.postback.query.action === 'getOrders') return true
-  return false
+const requiredParam = param => {
+  const requiredParamError = new Error(`Required parameter, "${param}" is missing.`)
+  // preserve original stack trace
+  if (typeof Error.captureStackTrace === 'function') {
+    Error.captureStackTrace(requiredParamError, requiredParam)
+  }
+  throw requiredParamError
 }
 
-const handleGetOrders = context => {
-  context.replyText('You click getOrders')
+const toColumn = product => {
+  return {
+    thumbnailImageUrl: product.imgUrl,
+    imageBackgroundColor: '#FFFFFF',
+    title: product.id + '. ' + product.name, // product.id just for teaching
+    text: product.detail,
+    defaultAction: {
+      type: 'postback',
+      label: '我想要買！',
+      text: '我想要買！',
+      data: `flow=shoping&action=buy&productID=${product.id}`
+    },
+    actions: [
+      {
+        type: 'postback',
+        label: '我想要買！',
+        text: '我想要買！',
+        data: `flow=shoping&action=buy&productID=${product.id}`
+      }
+    ]
+  }
+}
+
+const toColumns = ({
+  products = requiredParam('products'),
+  offset
+} = {}) => {
+  let columns = _.map(products, toColumn)
+  if (columns.length === 9 && offset) {
+    columns.push({
+      thumbnailImageUrl: 'https://i.imgur.com/0O9cN53.jpg',
+      imageBackgroundColor: '#ddc39d',
+      title: '想看更多嗎？', // product.id just for teaching
+      text: '還有更多好甜好吃的糖果唷(招手',
+      defaultAction: {
+        type: 'postback',
+        label: '我想看更多！',
+        text: '我想看更多！',
+        data: `flow=general&action=getProducts&offset=${offset}`
+      },
+      actions: [
+        {
+          type: 'postback',
+          label: '我想看更多！',
+          text: '我想看更多！',
+          data: `flow=general&action=getProducts&offset=${offset}`
+        }
+      ]
+    })
+  }
+  return columns
+}
+
+const handleGetProducts = async context => {
+  const {offset} = context.event.postback.query
+  const result = await Product.readProducts({offset})
+  console.log(result.offset)
+  let carouselProducts = {
+    type: 'template',
+    altText: '好吃好吃嚼嚼嚼',
+    imageAspectRatio: 'rectangle',
+    imageSize: 'cover',
+    template: {
+      type: 'carousel',
+      columns: toColumns({products: result.products, offset: result.offset})
+    }}
+  context.reply([carouselProducts])
 }
 
 const isGetProducts = context => {
@@ -18,46 +86,20 @@ const isGetProducts = context => {
   return false
 }
 
-const toColumns = products => {
-  return _.map(products, product => {
-    return {
-      thumbnailImageUrl: product.imgUrl,
-      imageBackgroundColor: '#FFFFFF',
-      title: product.name,
-      text: product.detail,
-      defaultAction: {
-        type: 'uri',
-        label: 'View image',
-        uri: product.imgUrl
-      },
-      actions: [
-        {
-          type: 'postback',
-          label: '我想要買！',
-          data: `flow=shoping&action=buy&productID=${product.id}`
-        }
-      ]
-    }
-  })
-}
-
-const handleGetProducts = async context => {
-  const products = await Product.readProducts()
-  let carouselProducts = {
-    type: 'template',
-    altText: '好吃好吃嚼嚼嚼',
-    imageAspectRatio: 'rectangle',
-    imageSize: 'cover',
-    template: {
-      type: 'carousel',
-      columns: toColumns(products)
-    }}
-  context.reply([carouselProducts])
-}
 const isGetMemberInfo = context => {
   const {event} = context
   if (event.postback.query.action && event.postback.query.action === 'getMemberInfo') return true
   return false
+}
+
+const isGetOrders = context => {
+  const {event} = context
+  if (event.postback.query.action && event.postback.query.action === 'getOrders') return true
+  return false
+}
+
+const handleGetOrders = context => {
+  context.replyText('You click getOrders')
 }
 
 const handleGetMemberInfo = context => {
