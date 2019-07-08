7997e41dd2ed80b6bd843ab051414d24ad21d2f1
CellBody <austintingwork@gmail.com>
2019-07-08 21:02:26 +0800

    Clean code

--- day-17/database/member.js
+++ day-17/database/member.js
@@ -12,12 +12,12 @@ const requiredParam = param => {
   throw requiredParamError
 }
 
-const updateMember = async ({id, name, phone, address}) => {
-  return airtable.upateRecord({table: 'member', id, data: {fields: {name, phone, address}}})
+const updateMember = async ({ id, name, phone, address }) => {
+  return airtable.updateRecord({ table: 'member', id, data: { fields: { name, phone, address } } })
 }
 
 const toMember = record => {
-  const {no, name, phone, address, line_id} = record.fields
+  const { no, name, phone, address, line_id } = record.fields
   return {
     id: record.id,
     no,
@@ -41,11 +41,11 @@ module.exports.createMember = async ({
   console.log(`record id: ${id}`)
   const filterByFormula = id ? `OR((RECORD_ID() = '${id}'), ({line_id} = '${lineID}'))` : `{line_id} = '${lineID}'`
   console.log(filterByFormula)
-  const memberResult = await airtable.getRecords({table: 'member', filterByFormula})
+  const memberResult = await airtable.getRecords({ table: 'member', filterByFormula })
   const members = _.map(memberResult.records, toMember)
   if (members.length > 1) return console.error(`發現雙胞胎啦! id: ${id}, lineID: ${lineID}, records: ${JSON.stringify(members)}`)
-  if (members.length === 1) return updateMember({id, name, phone, address})
-  const newMember = await airtable.createRecord({table: 'member', data: {fields: {line_id: lineID, name, phone, address}}})
+  if (members.length === 1) return updateMember({ id, name, phone, address })
+  const newMember = await airtable.createRecord({ table: 'member', data: { fields: { line_id: lineID, name, phone, address } } })
   newMember.fields.id = newMember.id
   return newMember.fields
 }
