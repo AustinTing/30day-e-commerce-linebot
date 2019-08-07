0b35cd347e8498d0a70ea7c3d5c661e0fa748de6
CellBody <austintingwork@gmail.com>
2019-08-07 17:45:46 +0800

    Add log and update displayText in handle_general_flow.

--- day-18/handlers/handle_general_flow.js
+++ day-18/handlers/handle_general_flow.js
@@ -48,14 +48,14 @@ const toColumns = ({
       defaultAction: {
         type: 'postback',
         label: '我想看更多！',
-        text: '我想看更多！',
+        displayText: '我想看更多！',
         data: `flow=general&action=getProducts&offset=${offset}`
       },
       actions: [
         {
           type: 'postback',
           label: '我想看更多！',
-          text: '我想看更多！',
+          displayText: '我想看更多！',
           data: `flow=general&action=getProducts&offset=${offset}`
         }
       ]
@@ -77,32 +77,47 @@ const handleGetProducts = async context => {
       columns: toColumns({ products: result.products, offset: result.offset })
     } }
   context.reply([carouselProducts])
+  console.log(`reply products list`)
+  console.log(`state: `, context.state)
 }
 
 const isGetProducts = context => {
   const { event } = context
-  if (event.postback.query.action && event.postback.query.action === 'getProducts') return true
+  if (event.postback.query.action && event.postback.query.action === 'getProducts') {
+    console.log(`handle_general_flow, isGetProducts`)
+    return true
+  }
   return false
 }
 
 const isGetMemberInfo = context => {
   const { event } = context
-  if (event.postback.query.action && event.postback.query.action === 'getMemberInfo') return true
+  if (event.postback.query.action && event.postback.query.action === 'getMemberInfo') {
+    console.log(`handle_general_flow, isGetMemberInfo`)
+    return true
+  }
   return false
 }
 
 const isGetOrders = context => {
   const { event } = context
-  if (event.postback.query.action && event.postback.query.action === 'getOrders') return true
+  if (event.postback.query.action && event.postback.query.action === 'getOrders') {
+    console.log(`handle_general_flow, isGetOrders`)
+    return true
+  }
   return false
 }
 
 const handleGetOrders = context => {
   context.replyText('You click getOrders')
+  console.log(`reply You click getOrders`)
+  console.log(`state: `, context.state)
 }
 
 const handleGetMemberInfo = context => {
   context.replyText('You click getMemberInfo')
+  console.log(`reply You click getOrders`)
+  console.log(`state: `, context.state)
 }
 
 module.exports = new LineHandler()
