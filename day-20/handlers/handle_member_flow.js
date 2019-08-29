35acc0b4ce972a4808922dd081e62d7f1cd50427
CellBody <austintingwork@gmail.com>
2019-08-29 18:42:08 +0800

    Add day-20

---
+++ day-20/handlers/handle_member_flow.js
@@ -0,0 +1,194 @@
+
+const { LineHandler } = require('bottender')
+const Member = require('../database/member.js')
+
+const isNewMember = context => {
+  if (!context.state.member) {
+    console.log('handle_member_flow, isNewMember, true')
+    return true
+  }
+  return false
+}
+
+const handleNewMember = async context => {
+  console.log('handle_member_flow, handleNewMember')
+  context.state.member = {}
+  context.state.flow = 'member_wait_input_name'
+  await context.pushText('Ops～小商發現您還沒註冊過唷，小商需要您的聯絡資訊才能送貨唷')
+  await context.pushText('請問您的姓名是？')
+  console.log('handle_member_flow, reply ask name')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const isWaitInputName = context => {
+  if (context.state.flow === 'member_wait_input_name') {
+    console.log('handle_member_flow, isWaitInputName, true')
+    return true
+  }
+  return false
+}
+
+const handleWaitInputName = async context => {
+  console.log('handle_member_flow, handleWaitInputName')
+  const { event } = context
+  if (!event.isText) return console.log(`Event is not text type. Evenet: ${event.rawEvent}`)
+  context.state.member.name = event.text
+  context.state.flow = 'member_wait_input_phone'
+  await context.replyText('好的！')
+  await context.pushText('當商品寄出時還需要您的手機號碼唷～請問您的手機號碼是？')
+  console.log('handle_member_flow, reply ask phone number')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const isWaitInputPhone = context => {
+  if (context.state.flow === 'member_wait_input_phone') {
+    console.log('handle_member_flow, isWaitInputPhone, true')
+    return true
+  }
+  return false
+}
+
+const handleWaitInputPhone = async context => {
+  console.log('handle_member_flow, handleWaitInputPhone')
+  const { event } = context
+  if (!event.isText) return console.log(`Event is not text type. Evenet: ${event.rawEvent}`)
+  context.state.member.phone = event.text
+  context.state.flow = 'member_wait_input_address'
+  await context.replyText('收到！')
+  await context.pushText('請問您的收貨地址是？')
+  console.log('handle_member_flow, reply ask address')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const isWaitInputAddress = context => {
+  if (context.state.flow === 'member_wait_input_address') {
+    console.log('handle_member_flow, isWaitInputAddress, true')
+    return true
+  }
+  return false
+}
+
+const handleWaitInputAddress = async context => {
+  console.log('handle_member_flow, handleWaitInputAddress')
+  const { event } = context
+  if (!event.isText) return console.log(`handle_member_flow, Event is not text type. Evenet: ${event.rawEvent}`)
+  context.state.member.address = event.text
+  context.state.flow = null
+  await context.reply([
+    {
+      type: 'template',
+      altText: '～',
+      template: {
+        type: 'buttons',
+        thumbnailImageUrl: 'https://i.imgur.com/dvYQD8J.jpg',
+        imageBackgroundColor: '#FFFFFF',
+        title: '聯絡資訊如下，我有記錯嗎？',
+        text: `姓名：${context.state.member.name}
+手機：${context.state.member.phone}
+寄件地址：${context.state.member.address}
+沒錯吧？`,
+        actions: [
+          {
+            type: 'postback',
+            label: '沒錯！',
+            displayText: '沒錯！',
+            data: `flow=member&action=confirm`
+          }, {
+            type: 'postback',
+            label: '不對唷～給我重來！',
+            displayText: '不對唷～給我重來！',
+            data: `flow=member&action=tryAgain`
+          },
+          {
+            type: 'postback',
+            label: '算了～下次再買吧～',
+            displayText: '算了～下次再買吧～',
+            data: `flow=member&action=cancelOrder`
+          }
+        ]
+      } }])
+  console.log('handle_member_flow, reply ask confirm')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const isWaitConfirm = context => {
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.flow === 'member') {
+    console.log('handle_member_flow, isWaitConfirm, true')
+    return true
+  }
+  return false
+}
+
+const isConfirm = context => {
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'confirm') {
+    console.log('handle_member_flow, isConfirm, true')
+    return true
+  }
+  return false
+}
+
+const handleConfirm = async context => {
+  console.log('handle_member_flow, handleConfirm')
+  context.state.flow = null
+  context.replyText('會員資訊已確認囉，接下來...')
+  context.state.member.lineID = context.session.user.id
+  // console.log(`member: ${JSON.stringify(context.state.member)}`)
+  // TODO: member checking
+  const newMember = await Member.createMember(context.state.member)
+  if (newMember) {
+    context.state.member = newMember
+  }
+  console.log('handle_member_flow, reply member info confirm')
+  console.log('handle_member_flow, final state: ', context.state)
+  // TODO: Set the pay flow state
+}
+
+const isTryAgain = context => {
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'tryAgain') {
+    console.log('handle_member_flow, isTryAgain, true')
+    return true
+  }
+  return false
+}
+
+const handleTryAgain = context => {
+  console.log('handle_member_flow, handleTryAgain')
+  context.state.flow = 'member_wait_input_name'
+  context.replyText('真不好意思>"<，請問您的姓名是')
+  console.log('handle_member_flow, reply ask name again')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const isCancelOrder = context => {
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'cancelOrder') {
+    console.log('handle_member_flow, isCancelOrder, true')
+    return true
+  }
+  return false
+}
+
+const handleCancelOrder = context => {
+  console.log('handle_member_flow, handleCancelOrder')
+  context.state({ flow: null, order: null })
+  context.replyText('嗚嗚～好的')
+  console.log('handle_member_flow, reply confirm cancel order')
+  console.log('handle_member_flow, final state: ', context.state)
+}
+
+const handleWaitConfirm = new LineHandler()
+  .on(isConfirm, handleConfirm)
+  .on(isTryAgain, handleTryAgain)
+  .on(isCancelOrder, handleCancelOrder)
+
+module.exports = new LineHandler()
+  .on(isNewMember, handleNewMember)
+  .on(isWaitConfirm, handleWaitConfirm)
+  .on(isWaitInputName, handleWaitInputName)
+  .on(isWaitInputPhone, handleWaitInputPhone)
+  .on(isWaitInputAddress, handleWaitInputAddress)
+  .onEvent(context => console.log('handle_member_flow, uncaught event:', context.event.rawEvent))
+  .build()
---
+++ day-20/handlers/handle_shopping_flow.js
@@ -0,0 +1,123 @@
+const { LineHandler } = require('bottender')
+const _ = require('lodash')
+const Product = require('../database/product.js')
+const handleMemberFlow = require('./handle_member_flow.js')
+
+const isBuy = context => {
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'buy') {
+    console.log('handle_shopping_flow, isBuy, true')
+    return true
+  }
+  return false
+}
+
+const handleBuy = async context => {
+  console.log('handle_shopping_flow, handleBuy')
+  const { productID } = context.event.postback.query
+  if (!productID) return console.log('there is no productID in postback!')
+  const product = await Product.readProduct({ id: productID })
+  if (!product || !product.isShow) return context.replyText('不好意思，已經買不到囉')
+  context.setState({ flow: 'shopping_wait_input_quantity', order: { productID: product.id } })
+  context.replyText(`想買${product.name}，好的，您想要買幾個呢？ (請輸入阿拉伯數字)`)
+  console.log('handle_shopping_flow, reply ask how many user want to buy')
+  console.log('handle_shopping_flow, final state: ', context.state)
+}
+
+const isWaitInputQuantityState = context => {
+  if (context.state.flow === 'shopping_wait_input_quantity') {
+    console.log('handle_shopping_flow, isWaitInputQuantityState, true')
+    return true
+  }
+  return false
+}
+
+const cancelBuy = context => {
+  console.log('handle_shopping_flow, cancelBuy')
+  context.state({ flow: null, order: null })
+  context.pushText('購買已先幫您取消囉')
+  context.pushText('再看看有沒有其他喜歡的吧～')
+  console.log('handle_shopping_flow, reply order has already cancel')
+  console.log('handle_shopping_flow, final state: ', context.state)
+}
+
+const handleWaitInputQuantityState = async context => {
+  console.log('handle_shopping_flow, handleWaitInputQuantityState')
+  const { event } = context
+  if (!event.isText || !/\d{1,}/g.test(event.text)) {
+    context.replyText('不好意思，小商只懂得0~9 >"<')
+    console.log('handle_shopping_flow, reply just know number')
+    console.log('handle_shopping_flow, final state: ', context.state)
+    return
+  }
+  const quantity = _.toInteger(event.text)
+  if (quantity === 0) return cancelBuy(context)
+  const product = await Product.readProduct({ id: context.state.order.productID })
+  if (!product || !product.isShow) {
+    context.replyText('不好意思，已經買不到囉')
+    console.log('handle_shopping_flow, reply cannot buy now')
+    console.log('handle_shopping_flow, final state: ', context.state)
+    return
+  }
+  context.state.order['quantity'] = quantity
+  context.state.flow = 'shopping_wait_confirm'
+  context.reply([{
+    type: 'template',
+    altText: '我確認一下唷～',
+    template: {
+      type: 'buttons',
+      thumbnailImageUrl: product.imgUrl,
+      imageBackgroundColor: '#FFFFFF',
+      title: '我確認一下唷～',
+      text: `您想要購買 ${product.name} ${quantity} 個，對吧？`,
+      actions: [
+        {
+          type: 'postback',
+          label: '沒錯！',
+          displayText: '沒錯！',
+          data: `flow=shopping&action=confirm`
+        }, {
+          type: 'postback',
+          label: '不對唷～',
+          displayText: '不對唷～',
+          data: `flow=shopping&action=cancel`
+        }
+      ]
+    } }])
+  console.log('handle_shopping_flow, reply confirm order')
+  console.log('handle_shopping_flow, final state: ', context.state)
+}
+
+const isWaitConfirm = context => {
+  if (context.state.flow === 'shopping_wait_confirm') {
+    console.log('handle_shopping_flow, isWaitConfirm, true')
+    return true
+  }
+  return false
+}
+
+const handleWaitConfirm = async context => {
+  console.log('handle_shopping_flow, handleWaitConfirm')
+  const { event } = context
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'confirm') {
+    console.log(`handle_shopping_flow, user confirm order`)
+    await context.replyText('購買資訊已確認，接下來...')
+    if (!context.state.member) {
+      return handleMemberFlow(context)
+    }
+    console.log('handle_shopping_flow, final state: ', context.state)
+    return // TODO: handlePayingFlow
+  }
+  if (event.isPostback && event.postback.query && event.postback.query.action === 'cancel') {
+    console.log(`handle_shopping_flow, user cancel order`)
+    cancelBuy(context)
+    // TODO: 再推一次商品列表
+  }
+}
+
+module.exports = new LineHandler()
+  .on(isBuy, handleBuy)
+  .on(isWaitInputQuantityState, handleWaitInputQuantityState)
+  .on(isWaitConfirm, handleWaitConfirm)
+  .onEvent(context => console.log('handle_shopping_flow, uncaught event:', context.event.rawEvent))
+  .build()
